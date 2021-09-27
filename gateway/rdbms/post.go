458cbdcfbb4d125a694a02eaaba7ebda99a756ed
raismaulana <lufriraismaulana@gmail.com>
2021-09-27 12:35:22 +0700

    fix: change fetch post by update to by create

--- gateway/rdbms/post.go
+++ gateway/rdbms/post.go
@@ -17,7 +17,7 @@ func (r *RDBMSGateway) FetchPosts(ctx context.Context, paginate database.Paginat
 	}
 
 	var objs []*entity.Post
-	err = db.Preload("Categories").Preload("Tags").Order("posts.updated_at desc").Scopes(database.Paginate(paginate)).Find(&objs).Error
+	err = db.Preload("Categories").Preload("Tags").Order("posts.created_at desc").Scopes(database.Paginate(paginate)).Find(&objs).Error
 	if err != nil {
 		log.Error(ctx, err.Error())
 		return nil, err
